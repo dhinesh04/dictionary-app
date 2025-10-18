b3180f64d7f4f5effe5d785615bcbabc08c03677
dhinesh04 <dhineshkumar4645@gmail.com>
2025-10-18 09:35:18 -0400

    Added CORS middleware in backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes import dictionary, words
 
 app = FastAPI(title = "Dictionary App")
+
+# Allow your frontend to talk to backend
+origins = [
+    "http://localhost:5173",  # React dev server
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # allow these origins
+    allow_credentials=True,
+    allow_methods=["*"],    # allow GET, POST, etc.
+    allow_headers=["*"],    # allow all headers
+)
 
 app.include_router(dictionary.router)
 app.include_router(words.router)
