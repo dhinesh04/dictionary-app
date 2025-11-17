a14c1c519da3c6dceee848015571f34a3ce0aad2
dhinesh04 <dhineshkumar4645@gmail.com>
2025-11-17 13:50:32 -0500

    transferred firebase config to env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,14 +9,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNh8-EVJ-pa_tQqqtfV9-nH94gh09SoSo",
-  authDomain: "dictionary-app-4645.firebaseapp.com",
-  projectId: "dictionary-app-4645",
-  storageBucket: "dictionary-app-4645.firebasestorage.app",
-  messagingSenderId: "99339215362",
-  appId: "1:99339215362:web:d84b24736d9127faaa3d1b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
